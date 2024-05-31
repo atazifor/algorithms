ae57d38b7a5496ca0abd17d48f3f1ee147f1883c
Amin Tazifor <amin.tazifor@gmail.com>
2024-05-31 08:46:03 +0100

    isMaxQueue, Iterator, etc added

--- 8puzzle/PriorityQueue.java
+++ 8puzzle/PriorityQueue.java
@@ -6,7 +6,9 @@
 
 import edu.princeton.cs.algs4.StdRandom;
 
-public class PriorityQueue<Key extends Comparable<Key>> {
+import java.util.Iterator;
+
+public class PriorityQueue<Key> implements Iterable<Key> {
 
     private final Key[] a;
     private int N;
@@ -17,7 +19,7 @@ public class PriorityQueue<Key extends Comparable<Key>> {
     }
 
     public void insert(Key key) {
-        a[N++] = key;
+        a[++N] = key;
         swim(N);
     }
 
@@ -29,6 +31,10 @@ public class PriorityQueue<Key extends Comparable<Key>> {
         return key;
     }
 
+    public int size() {
+        return N;
+    }
+
     public Key delRandom() {
         int k = StdRandom.uniformInt(1, N + 1);
         exch(1, k);
@@ -38,6 +44,10 @@ public class PriorityQueue<Key extends Comparable<Key>> {
     public Key sample() {
         int k = StdRandom.uniformInt(1, N + 1);
         return a[k];
+    }
+
+    public Iterator<Key> iterator() {
+        return new PriorityQueueIterator();
     }
 
     private void swim(int k) {
@@ -64,10 +74,65 @@ public class PriorityQueue<Key extends Comparable<Key>> {
     }
 
     private boolean less(int i, int j) {
-        return a[i].compareTo(a[j]) < 0;
+        return ((Comparable<Key>) a[i]).compareTo(a[j]) < 0;
+    }
+
+    // all children are definitely heaps of size 1.
+    // so we look at level height -1 to 1
+    public boolean isMaxHeap() {
+        return isHeapOrdered(1);
+    }
+
+    private boolean isHeapOrdered(int k) {
+        int left = 2 * k;
+        int right = left + 1;
+        if (left <= N && less(k, left)) return false;
+        else if (right <= N && less(k, right)) return false;
+        else return isHeapOrdered(left) && isHeapOrdered(right);
+    }
+
+    private void printQueue() {
+        for (Iterator<Key> it = iterator(); it.hasNext(); ) {
+            Key key = it.next();
+            System.out.print(" > " + key);
+        }
+        System.out.println();
+    }
+
+    private class PriorityQueueIterator implements Iterator<Key> {
+        PriorityQueue<Key> copy = null;
+
+        public PriorityQueueIterator() {
+            copy = new PriorityQueue<>(a.length);
+            for (int i = 1; i <= size(); i++) {
+                copy.insert(a[i]);
+            }
+        }
+
+        public Key next() {
+            if (hasNext()) return copy.delMax();
+            throw new UnsupportedOperationException("queue is empty");
+        }
+
+        public boolean hasNext() {
+            return copy.size() > 0;
+        }
+
     }
 
     public static void main(String[] args) {
 
+        PriorityQueue<Integer> q = new PriorityQueue<>(9);
+        q.insert(4);
+        q.insert(8);
+        q.insert(1);
+        q.insert(3);
+        q.printQueue();
+        System.out.println();
+        q.delMax();
+        q.delMax();
+        q.insert(5);
+        q.insert(10);
+        q.printQueue();
     }
 }
